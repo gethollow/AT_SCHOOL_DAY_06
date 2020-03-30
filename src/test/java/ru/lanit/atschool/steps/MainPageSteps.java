b8a51b2b5ca6384b45f13ac935b311e24cd2df1e
Hollow <edde.93@mail.ru>
2020-03-31 01:02:50 +0400

    [+] autorization test

--- src/test/java/ru/lanit/atschool/steps/MainPageSteps.java
+++ src/test/java/ru/lanit/atschool/steps/MainPageSteps.java
@@ -2,11 +2,12 @@ package ru.lanit.atschool.steps;
 
 import io.cucumber.java.ru.*;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import ru.lanit.atschool.webdriver.WebDriverManager;
 
-import java.util.HashMap;
+import java.util.Map;
 
 public class MainPageSteps {
     WebDriver driver = WebDriverManager.getDriver();
@@ -17,24 +18,23 @@ public class MainPageSteps {
         driver.get(url);
     }
 
-    @Пусть("пользователь вводит \"(.*)\"$")
-    public void userEnters(String arg0) {
-        driver = WebDriverManager.getDriver();
-        WebElement webElement = driver.findElement(By.name("q"));
+    @И("пользователь открыл форму авторизации")
+    public void userEnters() {
+        driver.findElement(By.xpath("//*[@id=\"user-menu-mount\"]/div/button[1]")).click();
     }
 
-    @И("заведен новый пользователь")
-    public void newUserRegistered() {
-    }
 
     @Тогда("пользователь вводит учетные данные")
-    public void userEnteredCredentials(HashMap<String, String> params) {
-        params.get("login");
-        params.get("password");
+    public void userEnteredCredentials(Map<String, String> params) {
+        driver.findElement(By.xpath("//*[@id=\"id_username\"]")).sendKeys(params.get("login"));
+        driver.findElement(By.xpath("//*[@id=\"id_password\"]")).sendKeys(params.get("password"));
     }
 
-    @И("вот")
-    public void vot() {
 
+    @Если("пользователь \"(.*)\" авторизован")
+    public void userSucces(String username) {
+        driver.findElement(By.xpath(""));
     }
+
+
 }
